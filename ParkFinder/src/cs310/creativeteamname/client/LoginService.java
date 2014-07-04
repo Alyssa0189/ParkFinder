7e2d1fb875ee20e006330db93ab1dd0d2ded159a
Tiffany Lam <w5p8@ugrad.cs.ubc.ca>
2014-07-04 13:25:12 -0700

    Implemented login service classes

--- ParkFinder/src/cs310/creativeteamname/client/LoginService.java
+++ ParkFinder/src/cs310/creativeteamname/client/LoginService.java
@@ -3,7 +3,10 @@ package cs310.creativeteamname.client;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
+import cs310.creativeteamname.shared.LoginInfo;
+
 @RemoteServiceRelativePath("login")
 public interface LoginService extends RemoteService {
+	public LoginInfo login(String requestUri);
 
 }
--- ParkFinder/src/cs310/creativeteamname/client/LoginServiceAsync.java
+++ ParkFinder/src/cs310/creativeteamname/client/LoginServiceAsync.java
@@ -1,5 +1,10 @@
 package cs310.creativeteamname.client;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import cs310.creativeteamname.shared.LoginInfo;
+
 public interface LoginServiceAsync {
+	public void login(String requestUri, AsyncCallback<LoginInfo> async);
 
 }
